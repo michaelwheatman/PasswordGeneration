75c62a49b220e1ba3e6378d56d5d2ce8cdd8bfcf
Michael Wheatman <michael.wheatman@gmail.com>
2016-09-20 18:47:09 -0400

    Cleared out old files

--- strengthen.py
+++ strengthen.py
@@ -2,14 +2,11 @@
 import sys
 import random
 
-letterOptions = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '^', '?', '\'', '"']
-def getRandomChar(r, prev):
-	return r.choice(letterOptions)
+#letterOptions = ['|', '}', '{', '>', '`', '^', '[', ':', '~', '%', ']', '<', '"', ';', "'", '(', 'Q', '?', ')', '=', '&', '+', '\\', ',', 'X', '$', '#', '/', 'Z', 'W', 'V', 'F', '@', 'J', '*', 'G', 'P', '-', 'U', '!', 'K', 'Y', 'H', 'q', 'B', '_', 'D', 'C', 'T', '.', 'M', 'S', 'N', 'R', 'O', 'L', 'I', 'E', 'x', 'A', 'z', 'w', 'f', 'v', 'j']
 
 def replace(word, replacements, r):
 	toReplace = r.sample(xrange(len(word)-1), replacements)
 	for i in toReplace:
-		#word = word[:i] + getRandomChar(r, word[i-1]) + word[i+1:]
 		word = word[:i] + r.choice(letterOptions) + word[i+1:]
 	return word
 
@@ -19,5 +16,6 @@ def main():
 			r = random.Random()
 			for line in f.readlines():
 				w.write(replace(line, 3, r))
-if __name__ == "__main__":
-	main()
+if __name__ == "__main__":	
+    letterOptions = ['|', '}', '{', '>', '`', '^', '[', ':', '~', '%', ']', '<', '"', ';', "'", '(', 'Q', '?', ')', '=', '&', '+', '\\', ',', 'X', '$', '#', '/', 'Z', 'W', 'V', 'F', '@', 'J', '*', 'G', 'P', '-', 'U', '!', 'K', 'Y', 'H', 'q', 'B', '_', 'D', 'C', 'T', '.', 'M', 'S', 'N', 'R', 'O', 'L', 'I', 'E', 'x', 'A', 'z', 'w', 'f', 'v', 'j']
+    main()
